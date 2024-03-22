8a4da19844d49d310073db4d8d9f5b3dfceeef07
BrytonNgaks <146178625+BrytonNgaks@users.noreply.github.com>
2024-03-22 23:57:19 +0300

    neew
    new

---
+++ login.js
@@ -0,0 +1,4 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    // Redirect to foodhubdemo.html
+    window.location.href = "foodhubdemo.html";
+});
---
+++ menu.js
@@ -0,0 +1,7 @@
+// script.js
+
+function redirectToFoodMenu() {
+    // Redirect to foodmenu.html
+    window.location.href = "foodmenu.html";
+    return false; // Prevent form submission
+}
